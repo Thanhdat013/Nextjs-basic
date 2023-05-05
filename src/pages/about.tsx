cfec91ca2663e6998c5e28cde36a2bc35a4c8a68
thanhdat <kimthanhdat0510@gmail.com>
2023-05-05 14:11:28 +0700

    SSG and Data Fetching on clinet side

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,14 +1,58 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import HeaderProps from './components/Header';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
 
 export interface AboutPage {}
+const HeaderProps = dynamic(() => import('@/components/Header'), { ssr: false });
 
 export default function AboutPage(props: AboutPage) {
+  const router = useRouter();
+  const [postList, setPostList] = useState([]);
+  const page = router.query?.page;
+  useEffect(() => {
+    if (!page) return;
+    (async () => {
+      const response = await fetch(`https://js-post-api.herokuapp.com/api/posts/?_page=${page}`);
+      const data = await response.json();
+
+      setPostList(data.data);
+    })();
+  }, [page]);
+
+  const handleClickNext = () => {
+    router.push(
+      {
+        pathname: 'about/',
+        query: {
+          page: Number(page || 1) + 1,
+        },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
+
   return (
     <div>
       <div>About page</div>
       <h3>Go to home</h3>
-      <Link href={'/'}></Link>
+      <HeaderProps />
+      <ul>
+        {postList.map((post: any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+      <button onClick={handleClickNext}>Next Page</button>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  console.log('render SSR');
+
+  return {
+    props: {},
+  };
 }
---
+++ src/pages/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export interface IHeaderprops {}
+const HeaderProps = () => {
+  console.log('render Header');
+
+  return <h1 style={{ color: '#FEFF86' }}> HeaderProps</h1>;
+};
+
+export default HeaderProps;
