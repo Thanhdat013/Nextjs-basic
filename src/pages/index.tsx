215d1475f92292cfa05c2ce47a35648ce5069a06
thanhdat <kimthanhdat0510@gmail.com>
2023-05-09 14:10:43 +0700

    hỗ trợ SEO cho trang Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,23 @@ import { Box } from '@mui/material'
 import HeroSection from '@/components/home/hero'
 import RecentPosts from '@/components/home/recentPosts'
 import FeatureWork from '@/components/home/featureWork'
+import Seo from '@/components/common/seo'
 
 const inter = Inter({ subsets: ['latin'] })
 
 const Home: NextPageWithLayout = () => {
   return (
     <Box>
+      <Seo
+        data={{
+          title: 'Next.js tutorials to beginner',
+          description:
+            'Step by step  tutorials to build a full CRUD website using Next.js for beginner',
+          url: 'https://nextjs-basic-kappa-eight.vercel.app/',
+          thumbnailUrl:
+            'https://res.cloudinary.com/dwgwlmu6i/image/upload/v1683616099/Next.Js_basic/image_p9tyze.jpg',
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeatureWork />
